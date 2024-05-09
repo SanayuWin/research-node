179ecfa6616fb6e5b4cc90fffefe953d87d53834
SanayuWin <sanayu_win@hotmail.com>
2024-05-09 23:44:30 +0700

    2024-05-09_3

--- frontend/src/pages/GenQRCode.js
+++ frontend/src/pages/GenQRCode.js
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-
-
 const GenQRCode = () => {
   const [qrCodeData, setQRCodeData] = useState(null);
   const BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
-  
+
   useEffect(() => {
-    fetchQRCode();
-  }, []);
+    const fetchQRCode = async () => {
+      try {
+        const response = await fetch(BACKEND_SERVER + "genqrcode");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-  const fetchQRCode = async () => {
-    try {
-      const response = await fetch(BACKEND_SERVER + "genqrcode");
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        const qrCodeBuffer = await response.blob();
+        setQRCodeData(URL.createObjectURL(qrCodeBuffer));
+      } catch (error) {
+        console.error("Error fetching QR code:", error);
       }
+    };
 
-      const qrCodeBuffer = await response.blob();
-      setQRCodeData(URL.createObjectURL(qrCodeBuffer));
-    } catch (error) {
-      console.error("Error fetching QR code:", error);
-    }
-  };
+    fetchQRCode();
+  }, [BACKEND_SERVER]); // Include BACKEND_SERVER in the dependency array
 
   return (
     <div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,34 +1,32 @@
 import React, { useState, useEffect } from "react";
 
-
 const Home = () => {
   const [data, setData] = useState([]);
   const BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
+
   useEffect(() => {
-    fetchData();
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(BACKEND_SERVER + "query");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(BACKEND_SERVER+"query");
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
+    };
 
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  
+    fetchData();
+  }, [BACKEND_SERVER]); 
+
   return (
     <div>
       {JSON.stringify(data)}
-      
     </div>
   );
 };
-
 
 export default Home;
